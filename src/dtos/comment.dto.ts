df6df072294fd8262e68eb3f0b4ccb5731be93f7
Valentina1012 <valentiki15@gmail.com>
2026-06-10 18:08:21 -0300

    refactor(comments): Add DTOs, interfaces and update model structure

---
+++ src/dtos/comment.dto.ts
@@ -0,0 +1,22 @@
+import type IComment from '../interfaces/IComment';
+import type { ICommentDocument } from '../models/Comment';
+
+export class CommentDto implements IComment {
+  id: string;
+  usuarioId: string;
+  productoId: string;
+  descripcion: string;
+  imagenes?: string[];
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(data: ICommentDocument) {
+    this.id = data._id.toString();
+    this.usuarioId = data.usuarioId.toString();
+    this.productoId = data.productoId.toString();
+    this.descripcion = data.descripcion;
+    this.imagenes = data.imagenes ?? [];
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
+  }
+}
